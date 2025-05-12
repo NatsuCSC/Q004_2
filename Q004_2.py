d286fbe7e0211ed86dcd3672a627f4f39b9685ba
ナツ <n.otani@cscloud.co.jp>
2025-05-12 12:52:05 +0900

    Initial commit

---
+++ Q004_2.py
@@ -0,0 +1,17 @@
+N, L = map(int, input().split())
+tranScripts = [list(map(int,input().split(" "))) for _ in range(L)]
+tempScript = []
+orderedScripts = []
+studentScripts = [0] * 5
+
+for subject in range(N):
+    tempScript = []
+    for person in range(L):
+        tempScript.append(tranScripts[person][subject])
+    else:
+        orderedScripts.append(tempScript)
+else:
+    for apexStudent in orderedScripts:
+        studentScripts[apexStudent.index(max(apexStudent))] += 1
+    else:
+        print(studentScripts.index(max(studentScripts)) + 1, max(studentScripts))
